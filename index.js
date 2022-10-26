1825122f02fd822d8ced191f8e2a9b7778b19a5c
Justin <91081414+cto-ai-justin@users.noreply.github.com>
2022-10-26 13:59:36 -0400

    Update index.js (#29)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000
 const HOST = '0.0.0.0'
 
 // Update RELEASE_NO below for Demos
-const RELEASE_NO = 'PROD-2022-210'
+const RELEASE_NO = 'PROD-2022-211'
 
 const api = express()
 api.set('views', path.join(__dirname, 'views'));
